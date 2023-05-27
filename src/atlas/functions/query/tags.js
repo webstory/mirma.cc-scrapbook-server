81a64329afbd4ecd474ea14e987c50f51c829ab8
Hoya Kim <wbstory@storymate.net>
2023-05-27 15:09:04 +0900

    Dev (#2)
    * mark hidden
    
    ---------
    
    Co-authored-by: mongodb-atlas-app-services[bot] <49962638+mongodb-atlas-app-services[bot]@users.noreply.github.com>

---
+++ src/atlas/functions/query/tags.js
@@ -0,0 +1,19 @@
+exports = function({query, headers, body}, response) {
+    let {tags, before} = query;
+    
+    if(!tags || tags.length === 0) {
+      return [];
+    }
+    
+    tags = tags.split(",").map((t)=>t.trim().toLowerCase());
+    if(!before || !/^\d+$/.test(before)) before = Date.now();
+    else before = Number(before);
+
+    const docs = context.services.get("mongodb-atlas").db("scrapbook").collection("files")
+    .find({ tags: { $all: tags }, create_timestamp: { $lte: before } })
+    .sort({ create_timestamp: -1 })
+    .limit(50)
+    .toArray();
+    
+    return docs;
+};
